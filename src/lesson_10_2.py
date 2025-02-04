694502a2ddca2a1f3beacfda7a1563b549688746
Alexei <streiker.saik@gmail.com>
2025-02-04 22:41:23 +0300

    update live-coding

--- src/lesson_10_2.py
+++ src/lesson_10_2.py
@@ -15,29 +15,43 @@ def calculate_taxes(prices: list[float], tax_rate: float) -> list[float]:
     return taxed_prices
 
 
-def calculate_tax(price: list[float], tax_rate: float, discount: float = 0.0) -> list[float]:
+def calculate_tax(
+        price: float,
+        tax_rate: float,
+        *,
+        discount: float = 0,
+        round_digits: int = 2
+) -> list[float]:
     """Функция вычисляет стоимость товара с учётом налога."""
 
-    if (
-        not isinstance(price, (int, float))
-        or not isinstance(tax_rate, (int, float))
-        or not isinstance(discount, (int, float))
-    ):
-        raise TypeError("Введены не цифровые значения")
+    for arg in [price, tax_rate, discount, round_digits]:
+        if not isinstance(arg, (int | float)):
+            raise TypeError("Ошибка типа данных")
+
+    # if (
+    #     not isinstance(price, (int, float))
+    #     or not isinstance(tax_rate, (int, float))
+    #     or not isinstance(discount, (int, float))
+    #     or not isinstance(round_digit, (int, float))
+    # ):
+    #     raise TypeError("Введены не цифровые значения")
 
     if price < 0:
         raise ValueError("Неверная цена")
 
-    if tax_rate <= 0 or tax_rate >= 100:
+    if tax_rate < 0 or tax_rate >= 100:
         raise ValueError("Неверный налоговый процент")
 
-    tax = price * tax_rate / 100
+    # tax = price * tax_rate / 100
+    #
+    # if discount == 0.0:
+    #     result = price + tax
+    #
+    # else:
+    #     price_discount = (price + tax) * discount / 100
+    #     result = price + tax - price_discount
 
-    if discount == 0.0:
-        result = price + tax
+    new_price = price + price * tax_rate / 100
+    price_with_discount = new_price - new_price * discount / 100
 
-    else:
-        price_discount = (price + tax) * discount / 100
-        result = price + tax - price_discount
-
-    return round(result, 2)
+    return round(price_with_discount, round_digits)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,17 @@
-import pytest
-
-@pytest.fixture
-def prices():
-    return [10.2, 5.8, 9.7]
-
-@pytest.fixture
-def tax_rate():
-    return 13.0
-
-@pytest.fixture
-def price():
-    return 10.2
-
-@pytest.fixture
-def discount():
-    return 10.0
+# import pytest
+#
+# @pytest.fixture
+# def prices():
+#     return [10.2, 5.8, 9.7]
+#
+# @pytest.fixture
+# def tax_rate():
+#     return 13.0
+#
+# @pytest.fixture
+# def price():
+#     return 10.2
+#
+# @pytest.fixture
+# def discount():
+#     return 10.0
